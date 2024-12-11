f785bdcae6926750d3c5cb5c821feee1beee06b3
koji-shimura <koji_shimura2@seesoft.co.jp>
2024-12-11 18:00:22 +0900

    add batch resources

--- modules/cloudwatch.tf
+++ modules/cloudwatch.tf
@@ -1,8 +1,8 @@
 ### cloudwatch log group
-resource "aws_cloudwatch_log_group" "log_group" {
-  name              = var.configs.cloudwatch.log.group_name
-  retention_in_days = var.configs.cloudwatch.log.retention_in_days
-  tags = {
-    Name = var.configs.cloudwatch.log.group_name
-  }
-}
+#resource "aws_cloudwatch_log_group" "log_group" {
+#  name              = var.configs.cloudwatch.log.group_name
+#  retention_in_days = var.configs.cloudwatch.log.retention_in_days
+#  tags = {
+#    Name = var.configs.cloudwatch.log.group_name
+#  }
+#}
