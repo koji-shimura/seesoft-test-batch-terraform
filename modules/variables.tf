6fe69da2fe148588b630991906bdea1c989cb728
koji-shimura <koji_shimura2@seesoft.co.jp>
2024-12-11 21:59:31 +0900

    add batch resources

--- modules/variables.tf
+++ modules/variables.tf
@@ -15,6 +15,18 @@ data "aws_subnet" "private_subnets" {
   }
 }
 
+# VPCエンドポイント(Gateway)用
+data "aws_route_tables" "route_tables" {
+  for_each = var.configs.route_tables_tagnames
+
+  vpc_id = var.vpc_id
+  filter {
+    name   = "tag:Name"
+    values = [each.value]
+  }
+}
+
+
 data "aws_iam_role" "batch_service_role" {
   name = "AWSBatchServiceRole"
 }
@@ -22,4 +34,9 @@ data "aws_iam_role" "batch_service_role" {
 # S3: task-policyが空だとダメなので（使わないけど)S3の権限を付与
 data "aws_s3_bucket" "test_bucket" {
   bucket = "test.seesoft.co.jp"
+}
+
+# エンドポイント用セキュリティグループ
+data "aws_security_group" "endpoint" {
+  id = var.configs.security_group_id_for_endpoint
 }
---
+++ modules/vpc_endpoint.tf
@@ -0,0 +1,69 @@
+### VPC Endpoint
+resource "aws_vpc_endpoint" "vpc_endpoints_for_interface" {
+  for_each = {
+    # for ECR
+    ecr_api = {
+      name        = "SS-Test-01.Inter3FargateTest_ep_ecrapi"
+      service     = "com.amazonaws.ap-northeast-1.ecr.api"
+    }
+    ecr_dkr = {
+      name        = "SS-Test-01.Inter3FargateTest_ep_ecrdkr"
+      service     = "com.amazonaws.ap-northeast-1.ecr.dkr"
+    }
+    log = {
+      name        = "SS-Test-01.Inter3FargateTest_ep_logs"
+      service     = "com.amazonaws.ap-northeast-1.logs"
+    }
+    # for ECS
+    ecs_agent = {
+      name        = "SS-Test-01.seesoft-test-batch_ecs-agent"
+      service     = "com.amazonaws.ap-northeast-1.ecs-agent"
+    }
+    ecs_tele = {
+      name        = "SS-Test-01.seesoft-test-batch_ecs-telemetry"
+      service     = "com.amazonaws.ap-northeast-1.ecs-telemetry"
+    }
+    ecs = {
+      name        = "SS-Test-01.seesoft-test-batch_ecs"
+      service     = "com.amazonaws.ap-northeast-1.ecs"
+    }
+  }
+
+  vpc_id            = var.vpc_id
+  service_name      = each.value.service
+  vpc_endpoint_type = "Interface"
+
+  security_group_ids = [
+    data.aws_security_group.endpoint.id,
+  ]
+
+  subnet_ids = [for subnet in data.aws_subnet.private_subnets : subnet.id]
+
+  private_dns_enabled = true
+
+  tags = {
+    Name = each.value.name
+  }
+
+}
+
+resource "aws_vpc_endpoint" "vpc_endpoints_for_gateway" {
+  for_each = {
+    s3 = {
+      name        = "SS-Test-01.Inter3FargateTest_ep_s3"
+      service     = "com.amazonaws.ap-northeast-1.s3"
+    }
+  }
+
+  vpc_id            = var.vpc_id
+  service_name      = each.value.service
+  vpc_endpoint_type = "Gateway"
+
+  route_table_ids = [for route_table in data.aws_route_tables.route_tables : route_table.id]
+
+  tags = {
+    Name = each.value.name
+  }
+
+}
+
