5e8fbf3fb750b39121d896b799a8065a12db732b
koji-shimura <koji_shimura2@seesoft.co.jp>
2024-12-13 13:12:14 +0900

    add lambda and event rule

--- modules/lambda.tf
+++ modules/lambda.tf
@@ -1,9 +1,9 @@
-resource "aws_lambda_function" "test_lambda_function" {
+resource "aws_lambda_function" "post_slack" {
   function_name    = "seesoft-test-batch-post-slack"
   filename         = "seesoft-test-batch-post-slack.zip"
   source_code_hash = data.archive_file.seesoft-test-batch-post-slack.output_base64sha256
   role             = aws_iam_role.roles["lambda"].arn
-  runtime          = "python3.13"
+  runtime          = "python3.9"
   handler          = "seesoft-test-batch-post-slack.lambda_handler"
   timeout          = 30
   environment {
